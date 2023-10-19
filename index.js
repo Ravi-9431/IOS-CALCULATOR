505e087f0da49c59c5310ff5e31db9e3ffa27189
Ravi-9431 <ravi4personal9431@gmail.com>
2023-10-19 08:52:45 +0530

    initial commit of ios calc

---
+++ index.js
@@ -0,0 +1,10 @@
+let displayValue = '';
+document.querySelector('#display').value=displayValue;
+// Get references to the calculator display and the Apple logo
+// const display = document.querySelector('#display');
+// const appleLogo = document.querySelector('#apple-logo');
+
+// // Add an event listener to the Apple logo
+// appleLogo.addEventListener('click', function () {
+//   display.value = ' '; // Clear the display
+// });
